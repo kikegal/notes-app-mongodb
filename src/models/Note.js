d077b0e231b24aa64beeccf4e32ba1fae78d1160
KikeGal <kikedosul@gmail.com>
2022-03-15 21:37:17 -0300

    config Note and User

--- src/models/Note.js
+++ src/models/Note.js
@@ -1,1 +1,17 @@
 const {Schema, model} = require('mongoose')
+
+const NoteSchema = new Schema({
+    title: {
+        type: String,
+        require: true
+     },
+
+     description: {
+         type: String,
+         require: true
+     }
+    },{
+        timestamps: true
+    })
+
+    module.export = model('Note', NoteSchema )
